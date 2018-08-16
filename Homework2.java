f17a0fc32c5d70e8da90fc62c8d1f2506d2141c9
HoneyAls <www.kozlov.a@mail.ru>
2018-08-16 20:57:33 +0300

    Homework2

---
+++ Homework2.java
@@ -0,0 +1,21 @@
+/**
+ * Java. Level 1. Lesson 2. Homework 2.
+ *
+*/
+class Homework2{
+    public static void main(String[] args){
+		
+	}
+	public static void changeValues(){
+		int[] t1 = {0, 1, 1, 0, 0, 1, 1}
+		for (int i = 0; i < t1.length; i++){
+			if(t1[i] == 0){
+				t1[i] = 1;
+			}
+			else{
+				t1[i] = 0
+			}
+		}
+	}
+}
+
