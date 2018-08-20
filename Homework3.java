648120d8f4e593c5db31e0b66af79bd55281de9d
HoneyAls <www.kozlov.a@mail.ru>
2018-08-20 14:56:53 +0300

    Homework3

--- Homework3.java
+++ Homework3.java
@@ -1,3 +1,34 @@
+/**
+ *
+ *
+ * 1.Write a program that guesses a random number from 0 to 9, and the user is 
+ * given 3 attempts to guess this number. Every time you try, the computer must 
+ * tell you whether the number specified by the user is more than the one conceived, 
+ * or less. After winning or losing a request is displayed - "Repeat the game again? 
+ * 1 - yes / 0 - no »(1 - repeat, 0 - no).
+ * 
+ * 2.Create array from the words: String[] words = {"apple", "orange", "lemon", "banana", 
+ * apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak",
+ * "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple",
+ * "pumpkin", "potato"}; 
+ * When the program is ran, the computer guess a word, asks the user for an answer,compares 
+ * it with the hidden word and tells whether the user answered correctly. If the word is not 
+ * guessed, the computer displays the letters that are in place. 
+ * apple - hidden;
+ * apricot - user answer;
+ * ap############# (15 symbols so that the user can not know the length of the word);
+ * To compare the two words character by character, you can use:
+ * String str = "apple";
+ * str.charAt(0); the method returns the char that is located in the word str in the 
+ * first position;
+ * Play till the user do not guess the word.
+ * Use just small letters.
+ *
+ *
+ *@author Alexandr Kozlov
+ *@version Aug 20, 2018
+*/
+
 import java.util.Scanner;
 import java.util.Random;
 import java.util.Arrays;
